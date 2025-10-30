77b8573fe1068c9ac93a95eaf19a97c8c73ab547
Alex Maynes <alex.maynes2001@gmail.com>
2025-10-29 22:55:20 -0600

    Some housekeeping

---
+++ src/utils/testImageSort.java
@@ -0,0 +1,31 @@
+package utils;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class testImageSort extends Application {
+    @Override
+    public void start(Stage stage) {
+        imageLoader loader = new imageLoader();
+        loader.loadImages();
+
+        FlowPane root = new FlowPane();
+        for (var img : loader.imageList) {
+            ImageView iv = new ImageView(img);
+            iv.setFitWidth(150);
+            iv.setPreserveRatio(true);
+            root.getChildren().add(iv);
+        }
+
+        stage.setScene(new Scene(root, 800, 600));
+        stage.setTitle("Sorted Images Test");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
