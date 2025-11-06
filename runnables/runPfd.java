1c905e133316e30053c1bfcac4c3efd18af4f8f5
Alex Maynes <alex.maynes2001@gmail.com>
2025-11-06 01:43:47 -0700

    new gui

--- runnables/runPfd.java
+++ runnables/runPfd.java
@@ -14,55 +14,5 @@ public class runPfd {
         Thread guiThread = new Thread(() -> Application.launch(gui.class));
         guiThread.start();
         Thread.sleep(3000); // Wait for GUI to initialize
-
-        int startingFloor = 6;
-
-        // API Calls
-        display.updateFloorIndicator(startingFloor, "IDLE");
-        panel.setDisplay(startingFloor, "IDLE");
-        display.playArrivalChime();
-        panel.resetFloorButton(startingFloor);
-        Thread.sleep(2000);
-        callButtons.pressUpCall();
-        Thread.sleep(2000);
-        callButtons.resetCallButton("UP");
-        Thread.sleep(2000);
-        for(int n = 0; n < 5; n++) {
-            int i = rand.nextInt(10);
-            display.playArrivalChime();
-            Thread.sleep(2000);
-            doors.open();
-            Thread.sleep(2000);
-            doors.setObstruction(true);
-            Thread.sleep(2000);
-            if(rand.nextInt(10) > 5) display.playOverLoadWarning(); // 50% chance to simulate weight overload call
-            Thread.sleep(2000);
-            doors.close();  // Should detect obstruction and reopen
-            Thread.sleep(2000);
-            doors.setObstruction(false);
-            Thread.sleep(2000);
-            doors.close();  // Now closes successfully
-            Thread.sleep(2000);
-
-            // Simulate passing floors
-            for(int j = 0; j < 3; j++) {
-                if(i > display.getCurrentFloor()) {
-                    display.updateFloorIndicator(i, "UP");
-                    panel.setDisplay(i, "UP");
-                } else if(i < display.getCurrentFloor()) {
-                    display.updateFloorIndicator(i, "DOWN");
-                    panel.setDisplay(i, "DOWN");
-                } else {
-                    continue;
-                }
-                Thread.sleep(1000);
-            }
-
-            panel.pressFloorButton(i);
-            Thread.sleep(2000);
-            display.updateFloorIndicator(i, "IDLE");
-            panel.setDisplay(i, "IDLE");
-            panel.resetFloorButton(i);
-        }
     }
 }
--- src/gui.java
+++ src/gui.java
@@ -8,9 +8,14 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.geometry.Pos;
@@ -35,8 +40,6 @@ public class gui extends Application {
     private listener listenerImpl;
     List<Label> buttonLabels = new ArrayList<>();
     ImageView elevDoorsImg = new ImageView();
-    ImageView floorDispImg = new ImageView();
-    ImageView elevPanelImg = new ImageView();
     ImageView elevCallButtonsImg = new ImageView();
 
     @Override
@@ -46,70 +49,39 @@ public class gui extends Application {
         loader.loadImages();
 
         elevDoorsImg.setPreserveRatio(true);
-        floorDispImg.setPreserveRatio(true);
-        elevPanelImg.setPreserveRatio(true);
         elevCallButtonsImg.setPreserveRatio(true);
         elevDoorsImg.setImage(loader.imageList.get(3)); // 3-7 indices are cabin doors
-        floorDispImg.setImage(loader.imageList.get(8)); // 8-10 indices are floor displays
-        elevPanelImg.setImage(loader.imageList.get(0)); // 0-2 indices are cabin panels
         elevCallButtonsImg.setImage(loader.imageList.get(11)); // 11-13 indices are call buttons
 
         // image scaling
-        elevPanelImg.setFitWidth(250);
-        floorDispImg.setFitWidth(120);
         elevDoorsImg.setFitWidth(400);
         elevCallButtonsImg.setFitWidth(120);
 
-        StackPane overlay1 = new StackPane(elevPanelImg);
-        StackPane overlay2 = new StackPane(floorDispImg);
-        StackPane.setAlignment(floorDispImg, Pos.TOP_CENTER);
-        HBox hbox = new HBox(0);
-        hbox.setAlignment(Pos.CENTER);
-        
-        Line divider = new Line(0, 0, 0, 800);
-        divider.setStrokeWidth(2);
-        divider.setStroke(Color.GRAY);
+        ScrollPane scrollPane = new ScrollPane();
+        VBox vbox = new VBox();
+        HBox hbox = new HBox();
 
-        // Button overlays
-        for (int i = 1; i < 10; i += 2) {
-            Label left = new Label(String.valueOf(i));
-            left.setStyle("-fx-text-fill: black;");
-            left.setFont(Font.font("Verdana", 16));
-            left.setTranslateX(-20);
-            left.setTranslateY(-52 + (17 * i));
-
-            // Capture final reference for lambda
-            left.setOnMouseClicked(event -> left.setStyle("-fx-text-fill: white;"));
-
-            overlay1.getChildren().add(left);
-            buttonLabels.add(left);
-
-            Label right = new Label(String.valueOf(i + 1));
-            right.setStyle("-fx-text-fill: black;");
-            right.setFont(Font.font("Verdana", 16));
-            right.setTranslateX(20);
-            right.setTranslateY(-52 + (17 * i));
-
-            right.setOnMouseClicked(event -> right.setStyle("-fx-text-fill: white;"));
-
-            overlay1.getChildren().add(right);
-            buttonLabels.add(right);
+        // Create floor displays & call buttons
+        for (int j = 0; j < 10; j++) {
+            HBox h = new HBox();
+            FloorDisplay floorDisplay = new FloorDisplay();
+            CallButton callButton = new CallButton();
+            h.getChildren().addAll(floorDisplay.floorDisplayOverlay, callButton.callButtonOverlay);
+            vbox.getChildren().add(h);
         }
-        floorLabel1 = new Label("-");
-        floorLabel1.setStyle("-fx-text-fill: white;");
-        floorLabel1.setFont(Font.font("Verdana", FontWeight.BOLD, 32));
-        floorLabel1.setTranslateY(-125);
-        overlay1.getChildren().add(floorLabel1);
-        floorLabel2 = new Label("-");
-        floorLabel2.setStyle("-fx-text-fill: white;");
-        floorLabel2.setFont(Font.font("Verdana", FontWeight.BOLD, 22));
-        floorLabel2.setTranslateY(-185);
-        overlay2.getChildren().add(floorLabel2);
+        scrollPane.setContent(vbox);
+        hbox.getChildren().add(scrollPane);
 
-        StackPane stacked = new StackPane(elevDoorsImg, floorDispImg, overlay2);
-        hbox.getChildren().addAll(stacked, elevCallButtonsImg, divider, overlay1);
+        // Create cabin panels & elevator doors
+        for (int i = 0; i < 4; i++) {
+            VBox v = new VBox();
+            Panel cabinPanel = new Panel();
+            ElevatorDoor elevatorDoor = new ElevatorDoor();
+            v.getChildren().addAll(cabinPanel.panelOverlay, elevatorDoor.doorOverlay);
+            hbox.getChildren().add(v);
+        }
 
-        Scene scene = new Scene(hbox, 770, 450, Color.web("#c0bfbbff"));
+        Scene scene = new Scene(hbox, 1800, 800, Color.web("#c0bfbbff"));
         primaryStage.setTitle("Elevator Passenger Devices");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -213,6 +185,125 @@ public class gui extends Application {
         }
     }
 
+    private class Panel{
+        private ImageView elevPanelImg = new ImageView();
+        public StackPane panelOverlay = new StackPane(elevPanelImg);
+        private double scaleFactor = 1;
+        private int yTranslation = -30; // adjust as needed for non 1:1 scales
+
+        private Panel(){ makePanel(); }
+
+        private void makePanel(){
+            elevPanelImg.setPreserveRatio(true);
+            elevPanelImg.setFitWidth(250 * scaleFactor);
+            elevPanelImg.setImage(loader.imageList.get(0)); // 0-2 indices are cabin panels
+
+            // Button overlays
+            for (int i = 1; i < 10; i += 2) {
+                Label left = new Label(String.valueOf(i));
+                left.setStyle("-fx-text-fill: black;");
+                left.setFont(Font.font("Verdana", 16  * scaleFactor));
+                left.setTranslateX(-20 * scaleFactor);
+                if(scaleFactor != 1) {
+                    left.setTranslateY(yTranslation + (17 * i) * scaleFactor); // adjust for scaling
+                } else {
+                    left.setTranslateY(-52 + (17 * i) * scaleFactor);
+                }
+
+                // Capture final reference for lambda
+                left.setOnMouseClicked(event -> left.setStyle("-fx-text-fill: white;"));
+
+                panelOverlay.getChildren().add(left);
+                buttonLabels.add(left);
+
+                Label right = new Label(String.valueOf(i + 1));
+                right.setStyle("-fx-text-fill: black;");
+                right.setFont(Font.font("Verdana", 16  * scaleFactor));
+                right.setTranslateX(20  * scaleFactor);
+                if(scaleFactor != 1) {
+                    right.setTranslateY(yTranslation + (17 * i) * scaleFactor); // adjust for scaling
+                } else {
+                    right.setTranslateY(-52 + (17 * i) * scaleFactor);
+                }
+
+                right.setOnMouseClicked(event -> right.setStyle("-fx-text-fill: white;"));
+
+                panelOverlay.getChildren().add(right);
+                buttonLabels.add(right);
+            }
+        }
+    }
+
+    private class FloorDisplay{
+        ImageView floorDispImg = new ImageView();
+        public StackPane floorDisplayOverlay = new StackPane(floorDispImg);
+
+        private FloorDisplay(){ makeDisplay(); }
+
+        private void makeDisplay(){
+            floorDispImg.setPreserveRatio(true);
+            floorDispImg.setFitWidth(120);
+            floorDispImg.setImage(loader.imageList.get(8)); // 8-10 indices are floor displays
+
+            Label floorLabel2 = new Label("1");
+            floorLabel2.setStyle("-fx-text-fill: white;");
+            floorLabel2.setFont(Font.font("Verdana", FontWeight.BOLD, 22));
+            floorLabel2.setTranslateY(-185);
+        }
+    }
+
+    private class ElevatorDoor{
+        ImageView elevDoorsImg = new ImageView();
+        public StackPane doorOverlay = new StackPane(elevDoorsImg);
+
+        private ElevatorDoor(){ makeDoor(); }
+
+        private void makeDoor(){
+            elevDoorsImg.setPreserveRatio(true);
+            elevDoorsImg.setFitWidth(400);
+            elevDoorsImg.setImage(loader.imageList.get(3)); // 3-7 indices are cabin doors
+        }
+    }
+
+    private class CallButton{
+        private ImageView elevCallButtonsImg = new ImageView();
+        public StackPane callButtonOverlay = new StackPane(elevCallButtonsImg);
+
+        private CallButton(){ makeCallButton(); }
+
+        private void makeCallButton(){
+            elevCallButtonsImg.setPreserveRatio(true);
+            elevCallButtonsImg.setFitWidth(100);
+            elevCallButtonsImg.setFitHeight(100);
+            elevCallButtonsImg.setImage(loader.imageList.get(11)); // 11-13 indices are call buttons
+            elevCallButtonsImg.setOnMouseClicked(event -> {
+
+            double clickY = event.getY();                    // where user clicked
+            double imgHeight = elevCallButtonsImg.getBoundsInLocal().getHeight();
+
+            if (clickY < imgHeight / 2) {
+                // Upper half clicked
+                elevCallButtonsImg.setImage(loader.imageList.get(13)); // top image
+            } else {
+                // Lower half clicked
+                elevCallButtonsImg.setImage(loader.imageList.get(12)); // bottom image
+            }
+        });
+        }
+    }
+
+    private class FireAlarm{
+        ImageView fireAlarmImg = new ImageView();
+
+        private FireAlarm(){ makeFireAlarm(); }
+
+        private void makeFireAlarm(){
+            fireAlarmImg.setPreserveRatio(true);
+            fireAlarmImg.setFitWidth(100);
+            fireAlarmImg.setImage(loader.imageList.get(14)); // 14-15 indices are fire alarms
+        }
+    }
+    
     // Change image in one of the 4 slots by index in the image list
     private void setImg(ImageView currImg, int newIndex) {
         try {
