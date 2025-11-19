f8abd81bc5d09cfea298e7ab67e971a86e82bc70
Joel Villarreal II <joel.villa2329@gmail.com>
2025-11-19 16:30:30 -0700

    comments

--- src/ElevatorController/ElevatorMain.java
+++ src/ElevatorController/ElevatorMain.java
@@ -4,8 +4,8 @@ import Bus.SoftwareBus;
 import ElevatorController.Processes.ElevatorController;
 
 /**
- * Main is a lightweight object, which instantiates Elevator Controller, Mode, Buttons, Cabin, Door Assembly and
- * Notifier.
+ * Main is a lightweight object, which instantiates Elevator Controller, Mode,
+ * Buttons, Cabin, Door Assembly and Notifier.
  */
 public class ElevatorMain {
     /**
