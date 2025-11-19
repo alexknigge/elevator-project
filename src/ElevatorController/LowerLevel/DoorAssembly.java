9533de8e64f255990271cb7ab9ab93129b43db7c
Joel Villarreal II <joel.villa2329@gmail.com>
2025-11-19 16:26:11 -0700

    comments

--- src/ElevatorController/LowerLevel/DoorAssembly.java
+++ src/ElevatorController/LowerLevel/DoorAssembly.java
@@ -2,9 +2,16 @@ package ElevatorController.LowerLevel;
 
 import Bus.SoftwareBus;
 
+/**
+ * The door assembly is a virtualization of the physical interfaces which
+ * comprise the doors: fully open sensors, fully closed sensors, door
+ * obstruction sensors, the scale, and the door motor. The door assembly posts
+ * and receives messages from its physical counterparts via the software bus;
+ * posting to the motor; receiving from the fully closed sensors, fully open
+ * sensors, the scale, and the door obstruction sensors.
+ */
 public class DoorAssembly implements Runnable {
-    //Not sure of we need opened and closed
-    private boolean opened;
+    private boolean opened;  // TODO: do we need to store this in Door Assembly?
     private boolean closed;
     private boolean obstructed;
     private boolean fullyClosed;
@@ -12,6 +19,11 @@ public class DoorAssembly implements Runnable {
     private boolean overCapacity;
     private SoftwareBus softwareBus;
 
+    /**
+     * Instantiate a DoorAssembly object, and run its thread
+     * @param elevatorNumber For software bus messages
+     * @param softwareBus The means of communication
+     */
     public DoorAssembly(int elevatorNumber, SoftwareBus  softwareBus) {
         //TODO may need to take in int for elevator number for software bus subscription
         //TODO call subscribe on softwareBus w/ relevant topic/subtopic
@@ -30,16 +42,37 @@ public class DoorAssembly implements Runnable {
     }
 
     //Todo: Write these methods
+    /**
+     * Send message to softwareBus to open the doors (which sends the message
+     * to the MUX)
+     */
     public void open(){}
 
+    /**
+     * Send message to softwareBus to close the doors (which sends the message
+     * to the MUX)
+     */
     public void close(){}
 
+    /**
+     * @return true if obstruction sensor triggered, false otherwise
+     */
     public boolean obstructed(){return obstructed;}
 
+    /**
+     * @return true if fully closed sensor triggered, false otherwise
+     */
     public boolean fullyClosed(){return fullyClosed;}
 
+    /**
+     * @return true if fully open sensor triggered, false otherwise
+     */
     public boolean fullyOpen(){return fullyOpen;}
 
+    /**
+     * @return true if an over capacity message was received, false if an under
+     *         capacity message was received, true initially
+     */
     public boolean overCapacity(){return overCapacity;}
 
     /**
