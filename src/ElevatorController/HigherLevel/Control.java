8338079ef45f6bade6de60810be72e954cc3df35
Joel Villarreal II <joel.villa2329@gmail.com>
2025-11-19 17:13:51 -0700

    skeleton for the higher level objects

---
+++ src/ElevatorController/HigherLevel/Control.java
@@ -0,0 +1,42 @@
+package ElevatorController.HigherLevel;
+
+import ElevatorController.LowerLevel.*;
+import ElevatorController.Util.State;
+
+/**
+ * Control mode is one where movement is controlled by the Control Room. Using
+ * the Software Bus. The Control Room can give commands to the elevators and
+ * assumes full control over the system.
+ */
+public class Control {
+    private Mode mode;
+    private Buttons buttons;
+    private Cabin cabin;
+    private DoorAssembly doorAssembly;
+    private Notifier notifier;
+
+    /**
+     * Create an instance of the Fire Object/Procedure
+     * @param mode the mode lower level object
+     * @param buttons the buttons lower level object
+     * @param cabin the cabin lower level object
+     * @param doorAssembly the door assembly lower level object
+     * @param notifier the notifier lower level object
+     */
+    public Control(Mode mode, Buttons buttons, Cabin cabin,
+                DoorAssembly doorAssembly, Notifier notifier) {
+        this.mode = mode;
+        this.buttons = buttons;
+        this.cabin = cabin;
+        this.doorAssembly = doorAssembly;
+        this.notifier = notifier;
+    }
+
+    /**
+     * Control mode implementation
+     * @return The state to switch too (normal or fire)
+     */
+    public State control(){
+        return null;
+    }
+}
---
+++ src/ElevatorController/HigherLevel/ElevatorController.java
@@ -0,0 +1,22 @@
+package ElevatorController.HigherLevel;
+
+import ElevatorController.LowerLevel.*;
+
+/**
+ * The elevator controller is a light-weight object responsible for switching
+ * between the elevator’s various modes.
+ */
+public class ElevatorController {
+    /**
+     * Create an instance of Elevator Controller
+     * @param normal the normal mode procedure
+     * @param fire the fire mode procedure
+     * @param control the controlled mode procedure
+     */
+    public ElevatorController(Normal normal, Fire fire, Control control) {}
+
+    /**
+     * Switch between the modes using their return values
+     */
+    public void elevatorController(){}
+}
