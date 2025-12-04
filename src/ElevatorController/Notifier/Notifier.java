77749c5e1d66b753564ca322bca7c8b9a5af014d
Kyle V Nguyen <vikhang04@unm.edu>
2025-12-03 19:39:45 -0700

    Update notifier

--- src/ElevatorController/Notifier/Notifier.java
+++ src/ElevatorController/Notifier/Notifier.java
@@ -1,6 +1,8 @@
 package ElevatorController.Notifier;
 
 import Bus.SoftwareBus;
+import Bus.SoftwareBusCodes;
+import Message.Message;
 import ElevatorController.Util.Destination;
 import ElevatorController.Util.State;
 
@@ -19,26 +21,52 @@ public class Notifier {
         this.currentElevatorId = currentElevatorId;
     }
 
+    /**
+     * sends a bus message to change the display to indicate that the
+     * elevator has arrived to a floor
+     *
+     * @param destination Direction and current floor
+     */
     public void arrivedAtFloor(Destination destination) {
 
+        Message arrrivedAtFloorMessage = new Message(SoftwareBusCodes.displayFloor, currentElevatorId, destination.getFloor());
+        softwareBus.publish(arrrivedAtFloorMessage);
     }
 
+    /**
+     * Send out current elevator status, specifically the direction the
+     * elevator is moving and its current floor position
+     * @param destination Direction and current floor
+     */
     public void elevatorStatus(Destination destination) {
+        int direction = destination.getDirection().getIntValue();
+        int floorLevel = destination.floor();
 
+        Message directionMessage = new Message(SoftwareBusCodes.displayDirection, currentElevatorId, direction);
+        Message currentPositionMessage = new Message(SoftwareBusCodes.cabinPosition, currentElevatorId, floorLevel);
+
+        Message commandCenterPosition = new Message(SoftwareBusCodes.elevatorStatus, currentElevatorId, floorLevel);
+        Message commandCenterDirection = new Message(SoftwareBusCodes.elevatorStatus, currentElevatorId, direction);
+
+        softwareBus.publish(directionMessage);
+        softwareBus.publish(currentPositionMessage);
+        softwareBus.publish(commandCenterPosition);
+        softwareBus.publish(commandCenterDirection);
     }
 
     /**
      * sends a bus message to play a noise because of too much weight
      */
     public void overloadOn() {
-
+        Message overLoadMessageOn = new Message(SoftwareBusCodes.playSound, currentElevatorId, 1);
+        softwareBus.publish(overLoadMessageOn);
     }
 
     /**
      * sends a bus message to stop playing noise
      */
     public void overloadOff() {
-
+        Message overLoadMessageOff;
     }
 
 
