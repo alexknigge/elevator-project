4bf8af05b50b5eda7c74f4c83c9df6aa8f2bfb9c
pandaweasel <ddavis11@unm.edu>
2025-12-03 19:50:39 -0700

    Update Mode.java
    mode

--- src/ElevatorController/Mode/Mode.java
+++ src/ElevatorController/Mode/Mode.java
@@ -17,6 +17,25 @@ public class Mode {
     private Destination currentDestination;
     private State currentMode;
     private int currentElevatorId;
+    // *** Topic Constants ***
+    // From Command Center to Mode
+    private static final int TOPIC_ON_OFF = SoftwareBusCodes.elevatorOnOff;
+    private static final int TOPIC_MODE = SoftwareBusCodes.setMode;
+    private static final int TOPIC_DESTINATION =
+            SoftwareBusCodes.setDestination;
+    // From Mode to Command Center
+    private static final int TOPIC_FIRE_MODE = SoftwareBusCodes.fireMode;
+
+    // From MUX to Mode
+    private static final int TOPIC_FIRE_ALARM =
+            SoftwareBusCodes.fireAlarmActive;
+    // From Mode to MUX
+    private static final int TOPIC_SET_FIRE = SoftwareBusCodes.fireAlarm;
+
+    // Body for mode changes
+    private static final int BODY_CENTRALIZED_MODE  = SoftwareBusCodes.centralized;
+    private static final int BODY_NORMAL_MODE = SoftwareBusCodes.normal;
+    private static final int BODY_FIRE_MODE = SoftwareBusCodes.fire;
 
     public Mode(SoftwareBus softwareBus, int currentElevatorId) {
         this.softwareBus = softwareBus;
@@ -33,7 +52,48 @@ public class Mode {
     }
 
     public State getMode(){
+        setCurrentMode();
         return currentMode;
+    }
+
+    private void setCurrentMode(){
+        Message modeMessage = softwareBus.get(currentElevatorId,TOPIC_MODE);
+        Message fireMessage = softwareBus.get(currentElevatorId,TOPIC_FIRE_ALARM);
+        Message statusMessage = softwareBus.get(currentElevatorId,TOPIC_ON_OFF);
+
+        int state;
+        if(modeMessage!=null){
+            state = modeMessage.getBody();
+            switch (state){
+                case BODY_CENTRALIZED_MODE -> currentMode = State.CONTROL;
+                case BODY_NORMAL_MODE -> currentMode = State.NORMAL;
+            }
+        }
+        if(fireMessage!=null){
+            state = fireMessage.getBody();
+            if (state == SoftwareBusCodes.pulled){
+                softwareBus.publish(new Message(TOPIC_FIRE_MODE, currentElevatorId,
+                        SoftwareBusCodes.emptyBody));
+                currentMode = State.FIRE;
+            }
+        }
+
+        if(statusMessage!=null){
+            state = statusMessage.getBody();
+            if (state == SoftwareBusCodes.off){
+                currentMode = State.OFF;
+            }
+        }
+
+
+        // Notify the MUX that the fire is active
+        if (currentMode == State.FIRE) {
+            //TODO are these bodies okay?
+            softwareBus.publish(new Message(TOPIC_SET_FIRE, currentElevatorId,
+                    SoftwareBusCodes.emptyBody));
+            softwareBus.publish(new Message(TOPIC_SET_FIRE, SoftwareBusCodes.buildingMUX, SoftwareBusCodes.emptyBody));
+        }
+
     }
 
     public Destination nextService() {
